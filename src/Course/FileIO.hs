c682600f715dcb3e7aca172d1c08c45edd775dcb
Yuriy Lazarev <unisay@users.noreply.github.com>
2019-12-16 22:07:51 +0100

    FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -81,50 +81,37 @@ the contents of c
 
 -- Given the file name, and file contents, print them.
 -- Use @putStrLn@.
-printFile ::
-  FilePath
-  -> Chars
-  -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile :: FilePath -> Chars -> IO ()
+printFile fp contents =
+  putStrLn ("============ " ++ fp) >>
+  putStrLn contents
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
-printFiles ::
-  List (FilePath, Chars)
-  -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles :: List (FilePath, Chars) -> IO ()
+-- printFiles = foldRight ((*>) . uncurry printFile) (pure ())
+printFiles = void . traverz (uncurry printFile)
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
-getFile ::
-  FilePath
-  -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile :: FilePath -> IO (FilePath, Chars)
+getFile fp = ((,) fp) <$> readFile fp
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
-getFiles ::
-  List FilePath
-  -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles :: List FilePath -> IO (List (FilePath, Chars))
+getFiles = traverz getFile  
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
-run ::
-  FilePath
-  -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run :: FilePath -> IO ()
+run = printFiles <=< getFiles . lines <=< readFile
 
 -- /Tip:/ use @getArgs@ and @run@
-main ::
-  IO ()
-main =
-  error "todo: Course.FileIO#main"
+main :: IO ()
+main = getArgs >>= \args -> case args of
+  Nil -> putStrLn "Please specify at least one argument"
+  h :. _ -> run h
 
 ----
 
@@ -132,3 +119,6 @@ main =
 -- ? `sequence . (<$>)`
 -- ? `void . sequence . (<$>)`
 -- Factor it out.
+
+traverz :: Applicative k => (a -> k b) -> List a -> k (List b)
+traverz = (sequence .) . (<$>)
